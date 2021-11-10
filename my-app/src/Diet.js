dbc5060e393d87a06cfc8d2445fbdc99758f9c29
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-10 14:50:23 +0530

    add some changes

--- my-app/src/Diet.js
+++ my-app/src/Diet.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Diet.css";
 import AutorenewIcon from "@material-ui/icons/Autorenew";
+// import CheckBoxIcon from "@material-ui/icons/CheckBox";
 import BlurCircularIcon from "@material-ui/icons/BlurCircular";
 // import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 function Diet() {
@@ -11,15 +12,23 @@ function Diet() {
       </div>
       <div className="breakfast">
         <h4>Breakfast</h4>
+        <small className="check">
+          <input type="checkbox" /> I ate this
+        </small>
+        {/* <button>
+          <CheckBoxIcon /> I ate this
+        </button> */}
         <AutorenewIcon />
         <BlurCircularIcon />
-        <img src="" alt="" />
-        <h5>2 Ingredient Protein Pancakes</h5>
-        {/* <select />
-        <option defaultValue>Select </option>
-        <option>gram</option>
-        <option>serving</option>
-        <select /> */}
+        <span>2 Ingredient Protein Pancakes</span>
+        <img
+          src="https://images.eatthismuch.com/site_media/thmb/412920_HillOK444_2d2221c9-dcb5-4249-827a-a413eae547e5.png"
+          alt="breakfast_image"
+        />
+        <select>
+          <option value="gram">gram</option>
+          <option value="serving">serving</option>
+        </select>
       </div>
       <div className="lunch">
         <h4>Lunch</h4>
@@ -27,12 +36,10 @@ function Diet() {
         <BlurCircularIcon />
         <img src="" alt="" />
         <h5>Raw Broccoli Salad</h5>
-        <select />
-        <option>Select </option>
-        <option>gram</option>
-        <option>serving</option>
-
-        {/* <select /> */}
+        <select>
+          <option value="gram">gram</option>
+          <option value="serving">serving</option>
+        </select>
       </div>
       <div className="dinner">
         <h4>Dinner</h4>
@@ -40,6 +47,10 @@ function Diet() {
         <BlurCircularIcon />
         <img src="" alt="" />
         <h5>Spicy Buttered Corn</h5>
+        <select>
+          <option value="gram">gram</option>
+          <option value="serving">serving</option>
+        </select>
       </div>
       <div className="snack">
         <h4>Snack</h4>
@@ -47,6 +58,10 @@ function Diet() {
         <BlurCircularIcon />
         <img src="" alt="" />
         <h5>Apple</h5>
+        <select>
+          <option value="gram">gram</option>
+          <option value="serving">serving</option>
+        </select>
       </div>
     </div>
   );
