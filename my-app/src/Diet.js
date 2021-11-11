bf6e3c7001074bc2840add6e69084b191b6297fb
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-11 13:43:41 +0530

    add some changes in css

--- my-app/src/Diet.js
+++ my-app/src/Diet.js
@@ -36,47 +36,76 @@ function Diet() {
           </select>
         </div>
       </div>
-      <div className="lunch">
+      <div className="breakfast">
         <h4>Lunch</h4>
-        <AutorenewIcon />
-        <BlurCircularIcon />
-        <img
-          src="https://images.eatthismuch.com/site_media/thmb/727026_Musni_f10b8cad-c042-45c3-a423-0a772dfa9de1.png"
-          alt="lunch_image"
-        />
-        <h5>Raw Broccoli Salad</h5>
-        <select>
-          <option value="gram">gram</option>
-          <option value="serving">serving</option>
-        </select>
+        <div className="breakfast1">
+          <small className="check">
+            <input type="checkbox" /> I ate this
+          </small>
+          <AutorenewIcon />
+          <BlurCircularIcon />
+        </div>
+        <div className="breakfast2">
+          <img
+            src="https://images.eatthismuch.com/site_media/thmb/727026_Musni_f10b8cad-c042-45c3-a423-0a772dfa9de1.png"
+            alt="lunch_image"
+          />
+          <h5> Raw Broccoli Salad</h5>
+        </div>
+        <div className="breakfast3">
+          <select>
+            <option value="gram">gram</option>
+            <option value="serving">serving</option>
+          </select>
+        </div>
       </div>
-      <div className="dinner">
+      <div className="breakfast">
+        {/* <div className="breakfast"> */}
         <h4>Dinner</h4>
-        <AutorenewIcon />
-        <BlurCircularIcon />
-        <img
-          src="https://images.eatthismuch.com/site_media/thmb/252806_simmyras_6dde2747-e548-45f3-ab32-781a5b397986.png"
-          alt="dinner"
-        />
-        <h5>Spicy Buttered Corn</h5>
-        <select>
-          <option value="gram">gram</option>
-          <option value="serving">serving</option>
-        </select>
+        {/* </div> */}
+        <div className="breakfast1">
+          <small className="check">
+            <input type="checkbox" /> I ate this
+          </small>
+          <AutorenewIcon />
+          <BlurCircularIcon />
+        </div>
+        <div className="breakfast2">
+          <img
+            src="https://images.eatthismuch.com/site_media/thmb/252806_simmyras_6dde2747-e548-45f3-ab32-781a5b397986.png"
+            alt="dinner"
+          />
+          <h5>Spicy Buttered Corn</h5>
+        </div>
+        <div className="breakfast3">
+          <select>
+            <option value="gram">gram</option>
+            <option value="serving">serving</option>
+          </select>
+        </div>
       </div>
-      <div className="snack">
+      <div className="breakfast">
         <h4>Snack</h4>
-        <AutorenewIcon />
-        <BlurCircularIcon />
-        <img
-          src="https://images.eatthismuch.com/site_media/thmb/473713_elm333_3173471a-a85f-4166-b97d-dd547cc4237f.jpg"
-          alt="apple_image"
-        />
-        <h5>Apple</h5>
-        <select>
-          <option value="gram">gram</option>
-          <option value="serving">serving</option>
-        </select>
+        <div className="breakfast1">
+          <small className="check">
+            <input type="checkbox" /> I ate this
+          </small>
+          <AutorenewIcon />
+          <BlurCircularIcon />
+        </div>
+        <div className="breakfast2">
+          <img
+            src="https://images.eatthismuch.com/site_media/thmb/473713_elm333_3173471a-a85f-4166-b97d-dd547cc4237f.jpg"
+            alt="apple_image"
+          />
+          <h5>Apple</h5>
+        </div>
+        <div className="breakfast3">
+          <select>
+            <option value="gram">gram</option>
+            <option value="serving">serving</option>
+          </select>
+        </div>
       </div>
     </div>
   );
