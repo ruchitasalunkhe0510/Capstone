5ec844620164840a2fe1bfa2e44449f1f7e49057
ruchitasalunkhe0510 <ruchitasalunkhe0510@gmail.com>
2021-11-16 13:01:45 +0530

    add some changes

--- my-app/src/Male.js
+++ my-app/src/Male.js
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 import React from "react";
 import ReactPlayer from "react-player";
 import './Male.css';
@@ -39,25 +33,3 @@ export default Male;
 
 
 
-// import React from "react";
-// import ReactPlayer from "react-player";
-// import "./Male.css";
-// function Male() {
-//   return (
-//     <div className="video-responsive">
-//       <h3>
-//         Workout videos -{" "}
-//         <a href="https://www.pexels.com/search/videos/weight%20loss/">
-//           Healthy Diet
-//         </a>
-//       </h3>
-//       <ReactPlayer url="https://youtu.be/ZrL3FNvSu1g" />
-//       <ReactPlayer url="https://youtu.be/aclHkVaku9U" />
-//       <ReactPlayer url="https://youtu.be/X1TuhAn6C-g" />
-//       <ReactPlayer url="https://youtu.be/GL1C6FCCKnQ" />
-//       <ReactPlayer url="https://youtu.be/qEmznUhN6IE" />
-//     </div>
-//   );
-// }
-
-// export default Male;
